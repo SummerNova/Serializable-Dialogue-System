3dd4c779fa9a0835c08c9cbc31185b27adfc9831
SummerNova <137454229+SummerNova@users.noreply.github.com>
2025-06-03 20:20:46 +0300

    Abstraction is working, fuck yeah

---
+++ Assets/Scripts/Scriptable Objects/AbstractSerializableDictionary.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "SerializableStringDictionary", menuName = "Scriptable Objects/SerializableStringDictionary"), System.Serializable]
+public abstract class AbstractSerializableDictionary<TKey,TValue,TEntry> : ScriptableObject, ISerializationCallbackReceiver 
+    where TEntry : AbstractEntry<TKey, TValue>, new()
+{
+    protected List<TEntry> SavedData
+    {
+        get => GetSavedData(); // implemented in concrete class
+        set => SetSavedData(value);
+    }
+
+    protected abstract List<TEntry> GetSavedData();
+    protected abstract void SetSavedData(List<TEntry> list);
+
+    protected Dictionary<TKey, TValue> RuntimeData = new();
+
+    public Dictionary<TKey, TValue> Data => RuntimeData;
+
+
+    /// <summary>
+    /// Load the Serialized list of data from the list into the dictionary. 
+    /// in other words, this is the READ of the data
+    /// </summary>
+    public void OnAfterDeserialize()
+    {
+        RuntimeData = new();
+        if (SavedData == null) return;
+        foreach (var abstractEntry in SavedData)
+        {
+            RuntimeData[abstractEntry.Key] = abstractEntry.Value;
+        }
+    }
+
+
+    /// <summary>
+    /// When unloading the SO, save the latest data from the dictionary into the saved data list. applies only in editor, as builds do not serialize.
+    /// in other words, this is the WRITE of the data
+    /// </summary>
+    public void OnBeforeSerialize()
+    {
+        if (SavedData == null)
+            SavedData = new List<TEntry>();
+        else
+            SavedData.Clear();
+
+        if (RuntimeData != null)
+        {
+            foreach (var KeyValuePair in RuntimeData)
+            {
+                TEntry newAbstractEntry = new();
+                newAbstractEntry.Set(KeyValuePair.Key, KeyValuePair.Value);
+                SavedData.Add(newAbstractEntry);
+            }
+        }
+    }
+}
+
+
+/// <summary>
+/// a Key and Value pair to save in the SavedData list
+/// </summary>
+public interface AbstractEntry<TKey,TValue>
+{
+    public TKey Key { get; set; }
+    public TValue Value { get; set; }
+
+    public void Set(TKey key, TValue value);
+    
+}
--- Assets/Scripts/Scriptable Objects/SerializableStringDictionary.cs
+++ Assets/Scripts/Scriptable Objects/SerializableStringDictionary.cs
@@ -2,51 +2,13 @@ using UnityEngine;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "SerializableStringDictionary", menuName = "Scriptable Objects/SerializableStringDictionary"), System.Serializable]
-public class SerializableStringDictionary : ScriptableObject, ISerializationCallbackReceiver
+public class SerializableStringDictionary : AbstractSerializableDictionary<int,string,intStringEntry>, ISerializationCallbackReceiver
 {
     [SerializeField]
-    private List<Entry> SavedData;
+    private List<intStringEntry> _savedData;
 
-    private Dictionary<int, string> RuntimeData = new();
-
-    public Dictionary<int, string> Data => RuntimeData;
-
-
-    /// <summary>
-    /// Load the Serialized list of data from the list into the dictionary. 
-    /// in other words, this is the READ of the data
-    /// </summary>
-    public void OnAfterDeserialize()
-    {
-        RuntimeData = new();
-        if (SavedData == null) return;
-        foreach (var entry in SavedData)
-        {
-            RuntimeData[entry.Key] = entry.Value;
-        }
-    }
-
-
-    /// <summary>
-    /// When unloading the SO, save the latest data from the dictionary into the saved data list. applies only in editor, as builds do not serialize.
-    /// in other words, this is the WRITE of the data
-    /// </summary>
-    public void OnBeforeSerialize()
-    {
-        if (SavedData == null)
-            SavedData = new List<Entry>();
-        else
-            SavedData.Clear();
-
-        if (RuntimeData != null)
-        {
-            foreach (var KeyValuePair in RuntimeData)
-            {
-                Entry newEntry = new(KeyValuePair.Key, KeyValuePair.Value);
-                SavedData.Add(newEntry);
-            }
-        }
-    }
+    protected override List<intStringEntry> GetSavedData() => _savedData;
+    protected override void SetSavedData(List<intStringEntry> list) => _savedData = list;
 }
 
 
@@ -54,14 +16,23 @@ public class SerializableStringDictionary : ScriptableObject, ISerializationCall
 /// a Key and Value pair to save in the SavedData list
 /// </summary>
 [System.Serializable]
-public struct Entry
+public struct intStringEntry : AbstractEntry<int,string>
 {
-    public int Key;
-    public string Value;
+    [SerializeField]
+    public int _key;
+    [SerializeField]
+    public string _value;
 
-    public Entry(int key, string value)
+    public int Key { get { return _key; } set { _key = value; } }
+    public string Value
     {
-        Key = key;
-        Value = value;
+        get { return _value; }
+        set { _value = value; }
+    }
+
+    public void Set(int key, string value)
+    {
+        _key = key;
+        _value = value;
     }
 }
