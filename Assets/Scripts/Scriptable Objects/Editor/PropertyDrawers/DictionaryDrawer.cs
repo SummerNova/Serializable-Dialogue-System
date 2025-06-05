c7abbca9cb028b4ffcb1f3146b12838c0bc74782
Sagrius <alexeibuic@gmail.com>
2025-06-05 21:21:20 +0300

    made a drawer for enum with color

---
+++ Assets/Scripts/Scriptable Objects/Editor/PropertyDrawers/DictionaryDrawer.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using static CharacterDialogue;
+
+[CustomPropertyDrawer(typeof(DictionaryType))]
+public class DictionaryDrawer:PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+        DictionaryType type = (DictionaryType)property.enumValueIndex;
+        Color defaultColor = GUI.color;
+        GUI.color = GetTypeColor(type);
+        property.enumValueIndex = EditorGUI.Popup(position,
+            label.text, property.enumValueIndex, property.enumDisplayNames);
+        GUI.color = defaultColor;
+        EditorGUI.EndProperty();
+    }
+
+    private Color GetTypeColor(DictionaryType Type)
+    {
+        switch (Type)
+        {
+            case DictionaryType.All:
+                return Color.white; 
+            case DictionaryType.AudioClip:
+                return Color.green; 
+            case DictionaryType.Text:
+                return Color.cyan;
+            case DictionaryType.Texture:
+                return Color.yellow;
+            default:
+                return Color.gray; 
+        }
+    }
+
+}
