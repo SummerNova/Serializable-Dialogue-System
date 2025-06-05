5afdf2c0bb66b3cf6677cd5c46b6f98cb4694123
SummerNova <137454229+SummerNova@users.noreply.github.com>
2025-06-05 23:22:36 +0300

    list compatible property drawer

--- Assets/Scripts/Scriptable Objects/Editor/PropertyDrawers/DialogueValidityDrawer.cs
+++ Assets/Scripts/Scriptable Objects/Editor/PropertyDrawers/DialogueValidityDrawer.cs
@@ -8,34 +8,34 @@ public class DialogueValidityDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        // Start property block
         EditorGUI.BeginProperty(position, label, property);
+
 
         float lineHeight = EditorGUIUtility.singleLineHeight;
         float spacing = EditorGUIUtility.standardVerticalSpacing;
 
-        // Define layout: two lines
+        
         Rect labelRow = new Rect(position.x , position.y, position.width, lineHeight);
         Rect fieldRow = new Rect(position.x , position.y + lineHeight + spacing, position.width, lineHeight);
 
         float width = fieldRow.width / 6;
         float space = fieldRow.width / 12;
-        float xOffset = fieldRow.width /6;
+        float xOffset = fieldRow.width /4;
 
         Rect IndexRect = new Rect(position.x, position.y + lineHeight / 2 + spacing / 2, xOffset, lineHeight);
 
-        // Get properties
+        EditorGUI.PrefixLabel(IndexRect, label);
+
+        
         SerializedProperty textureID = property.FindPropertyRelative("TextureID");
         SerializedProperty dialogueID = property.FindPropertyRelative("DialogueID");
         SerializedProperty voiceLineID = property.FindPropertyRelative("VoiceLineID");
 
 
-        // Rects for each label
         Rect textureLabelRect = new Rect(labelRow.x + xOffset , labelRow.y, width, lineHeight);
         Rect dialogueLabelRect = new Rect(labelRow.x + space + width + xOffset, labelRow.y, width, lineHeight);
         Rect voiceLabelRect = new Rect(labelRow.x + 2 * (width + space) + xOffset, labelRow.y, width, lineHeight);
 
-        // Rects for each field
         Rect textureFieldRect = new Rect(fieldRow.x + xOffset , fieldRow.y, width, lineHeight);
         Rect dialogueFieldRect = new Rect(fieldRow.x + width + space + xOffset, fieldRow.y, width, lineHeight);
         Rect voiceFieldRect = new Rect(fieldRow.x + 2 * (width + space) + xOffset, fieldRow.y, width, lineHeight);
@@ -50,22 +50,12 @@ public class DialogueValidityDrawer : PropertyDrawer
             alignment = TextAnchor.MiddleCenter
         };
 
-        //string path = property.propertyPath;
-        //var match = Regex.Match(path, @"[(\d+)]");
-        //if (match.Success)
-        //{
-        //    int index = int.Parse(match.Groups[1].Value);
-        //    string labelText = $"Element {index}";
-        //    EditorGUI.LabelField(IndexRect, labelText);
-        //}
+        
 
-
-        // Draw centered labels
         EditorGUI.LabelField(textureLabelRect, "Texture ID", centeredLabel);
         EditorGUI.LabelField(dialogueLabelRect, "Dialogue ID", centeredLabel);
         EditorGUI.LabelField(voiceLabelRect, "VoiceLine ID", centeredLabel);
 
-        // Draw centered int fields manually
         textureID.intValue = EditorGUI.IntField(textureFieldRect, textureID.intValue, centeredField);
         dialogueID.intValue = EditorGUI.IntField(dialogueFieldRect, dialogueID.intValue, centeredField);
         voiceLineID.intValue = EditorGUI.IntField(voiceFieldRect, voiceLineID.intValue, centeredField);
